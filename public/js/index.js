df8d06a9624793daac6918e2ec2d7830c13ba62d
yl3030 <yl301114@gmail.com>
2021-05-06 00:50:26 +0800

    0505

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,17 @@
 var SecondMenuNum = $(".second-menu").children("li").length;
 var SecondMenuWidth = 100/SecondMenuNum;
 $(".second-menu").children("li").css("width",SecondMenuWidth+"%");
+
+// new
+$(".tab-box").children("button").click(function(){
+    $(".tab-box").children("button").removeClass("active")
+    $(this).addClass("active");
+    $(".new-content").hide();
+    if($(this).hasClass("ad")){
+        $(".new-content.new-ad").show();
+    }else if($(this).hasClass("speech")){
+        $(".new-content.new-speech").show();
+    }else if($(this).hasClass("paper")){
+        $(".new-content.new-paper").show();
+    }
+})
